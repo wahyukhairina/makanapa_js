336e9f09af0ebb74cb30dc602f70d9f603119e9a
wahyukhairina <wkhairina.afzan@gmail.com>
2020-06-11 10:30:00 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="container">
+      <div>
+        Navbar
+      </div>
+      <div className="row">
+        <div className='column1' style={{backgroundColor:'red'}}>
+        Content 1
+        </div>
+        <div className='column2' style={{backgroundColor:'blue'}}>
+        Content 2
+        </div>
+      </div>
+      </div>
+    </>
   );
 }
 
