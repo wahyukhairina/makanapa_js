686a964a6dd1dbb263fe12cda0cdef3bf8522d41
wahyukhairina <wkhairina.afzan@gmail.com>
2020-06-11 10:52:52 +0800

    main template

--- src/App.js
+++ src/App.js
@@ -3,16 +3,32 @@ import './App.css';
 function App() {
   return (
     <>
-    <div className="container">
-      <div>
+    <div className='container-main'>
+      <div className='navbar'>
         Navbar
       </div>
-      <div className="row">
-        <div className='column1' style={{backgroundColor:'red'}}>
-        Content 1
+      <div className='container-content'>
+        <div className='container-left'>
+          <div className='header-image'>
+            Header Image
+          </div>
+          <div className='category'>
+            Category
+          </div>
+          <div className='product-list'>
+            Product List
+          </div>
         </div>
-        <div className='column2' style={{backgroundColor:'blue'}}>
-        Content 2
+        <div className='container-right'>
+          <div className='address'>
+            Address
+          </div>
+          <div className='cart'>
+            Cart
+          </div>
+          <div className='total'>
+            Total
+          </div>
         </div>
       </div>
       </div>
